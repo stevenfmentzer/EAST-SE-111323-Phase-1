c0e519e17c856f221c45f0a8ae84b2e335967ddb
Steven Mentzer <stevenfmentzer@gmail.com>
2023-11-13 12:36:27 -0500

    solution code added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-console.log("testing")
+const value = "immediately assigned a value that doesn't change";
+let value2 = "";
+let value3 = "Can be assigned empty or immediatley. Can be reassigned";
+const number = 49; 
+const boolean = true; 
+const array = ['thing1','thing2'];
 
-// write your code here
+//How do we inspect the value of the variable?
+
+console.log(value, number, boolean, array);
+
+
+
+// Deliverable 1
+const greeting = "Welcome to Flatburger!";
+// Deliverable 2
+let num1 = 10;
+let num2 = 20;
+// Deliverable 3
+let sum = num1 + num2;
+// Deliverable 4
+let sumString = `${num1} + ${num2} = ${sum}`;
+console.log(sumString);
+// Deliverable 5 & 6
+if (num1 === 7 || num1 === 49){
+    console.log("That's a lucky number!");
+} else { 
+    console.log("That's not a lucky number");
+}
