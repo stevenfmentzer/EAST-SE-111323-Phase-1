58147df76710b98ba2e68311cd581b31efa63658
Steven Mentzer <stevenfmentzer@gmail.com>
2023-11-15 14:29:18 -0500

    index.js added deliverables and lecture notes

--- src/index.js
+++ src/index.js
@@ -55,3 +55,104 @@ const otherFoods = [
 ]
 
 // write your code here
+
+// .getElementById() - find 1 element by its ID 
+// const restaurantMenu = document.getElementById('restaurant-menu')
+// const h1 = document.createElement('h1')
+// console.log(restaurantMenu)
+
+// .querySelector() - finds 1 element
+// const detailImage = document.querySelector('.detail-image')
+// console.log(detailImage)
+
+// // . querySelectorAll()
+// const divElements = document.querySelectorAll('div')
+// console.log(divElements)
+// //  This returns a 'NodeList' that we can iterate through using a For Loop
+
+//  // console.log(divElements[0])
+//  for (let index = 0 ; index < divElements.length ; index++){
+//     console.log(divElements[index])
+//  }
+
+// divElements.forEach((div) => {
+// console.log(divElements)
+//})
+
+//  // .getElementsByClassName() 
+// const nameElements = document.getElementsByClassName('name')[0] //Using [0] gets the first element it finds
+// console.log(nameElements)
+
+// for (let index = 0 ; index < nameElements.lenght ; index ++){
+//     console.log(nameElements[index])
+// }
+
+// // For Loop for indexing all the burgers. This works but is redundant (SEE BELOW '.forEach')
+// for ( let index = 0 ; index < burgers.length ; index ++){
+//     console.log (burgers[index])
+// }
+
+// burgers.forEach((burger) => {
+// console.log(`${burger.name}  :  ${burger.image}`)
+// })
+
+// // This .forEach is basicallly a for loop that will provide its own 'index' count
+// burgers.forEach((burger, index) => {
+//     console.log(index)
+//     console.log(burger)
+// })
+
+
+
+//DELIVERABLES
+
+
+//Deliverable 1 
+const restaurantMenu = document.getElementById('restaurant-menu')
+
+for (let index = 0 ; index < burgers.length ; index ++){
+    const imgElement = document.createElement('img');
+    imgElement.className = 'burger';
+    imgElement.src = burgers[index].image;
+    restaurantMenu.appendChild(imgElement);   
+}
+
+//Deliverable 2
+otherFoods.forEach((food) => {
+    const imgElement = document.createElement('img')
+    imgElement.src = food.image
+    restaurantMenu.appendChild(imgElement); 
+})
+//Deliverable 3 
+
+const detailImage2 = document.querySelector('.detail-image')
+detailImage2.src = burgers[0].image
+
+//Deliverable 4 
+
+const burgerName = document.querySelector('.name')
+burgerName.textContent = burgers[0].name
+
+//Deliverable 5
+
+const description = document.querySelector('#description-display')
+description.textContent = burgers[0].description
+
+//Deliverable 6
+const restMenuImg = document.getElementById("restaurant-menu").querySelectorAll("img")
+
+restMenuImg.forEach((element) => {
+    element.style.borderStyle = 'solid';
+    element.style.borderColor = 'red';
+    element.style.borderWidth = '3px';
+})
+
+//Deliverable 7
+const burgerImg = document.getElementsByClassName("burger")
+
+for (let index = 0 ; index < burgerImg.length ; index ++ ){
+    burgerImg[index].style.borderColor = 'blue';
+}
+
+
+
